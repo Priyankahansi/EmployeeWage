48b749e1e6e7d21bf0936454706281083f41952a
Priyanka <priyankahansi95@gmail.com>
2022-04-20 17:48:23 +0530

    added employee wages for totalworking hour per month is reached

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -7,12 +7,15 @@ public class EmployeeWage {
 
     public static final int NUM_OF_WORKING_DAYS = 2;
 
+    public static final int MAX_HRS_IN_MONTH = 10;
+
     public static void main(String[] args) {
         int empHrs = 0;
-        int empWage = 0;
-        int totalEmpWage = 0;
+        int totalEmpHrs = 0;
+        int totalWorkingDays = 0;
 
-        for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+            totalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empCheck) {
                 case IS_PART_TIME:
@@ -25,12 +28,11 @@ public class EmployeeWage {
                     empHrs = 0;
             }
 
-            empWage = empHrs * EMP_RATE_PER_HOUR;
 
-            System.out.println("Emp wage:" + empWage);
-            totalEmpWage += empWage;
-            System.out.println("Emp Wage:" + empWage);
+            totalEmpHrs += empHrs;
+            System.out.println("day#:" + totalWorkingDays + "Emp HI:" + empHrs);
         }
+        int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
         System.out.println("Total Emp Wage:" + totalEmpWage);
     }
 }
